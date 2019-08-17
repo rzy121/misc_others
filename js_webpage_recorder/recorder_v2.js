f42d7c1f5969d2452ae7a8d64bc24b3d85caaa08
Rayne Zhang <raynezhang121@gmail.com>
2019-08-17 00:08:30 -0700

    split recorder functions to buttons in v2.js

--- js_webpage_recorder/recorder_v2.js
+++ js_webpage_recorder/recorder_v2.js
@@ -1,21 +1,62 @@
-const player = document.getElementsById('player');
+// binding html elements to variables
+const startButton = document.getElementById('btn_start');
+const stopButton = document.getElementById('btn_stop');
+//const playButton = document.getElementById('btn_play');
+const submitButton = document.getElementById('btn_send');
+const player = document.getElementById('player');
 
-const mic = stream => {
+// initialize new recorder object, and takes the stream object as input
 
-  const context = new AudioContext();
-  const source = context.createMediaStreamSource(stream);
-  const processor = context.createScriptProcessor(1024, 1, 1);
+stopButton.disabled = true;
+submitButton.disabled = true;
 
-  source.connect(processor);
-  processor.connect(context.destination);
+const mic = async () => {
 
-  porcessor.onaudioprocess = e => {
+  // request access to mic; if granted, return a stream object
+  const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false});
 
-    console.log(e.inputButter);
+  // initialize empty array to store recorded data
+  const recorder = new MediaRecorder(stream);
 
-  };
+  // initialize empty array to store recording
+  var audioChunks = [];
 
+  startButton.addEventListener('click', () => {
+    // send a signal for recorder to start
+    recorder.start();
+    console.log(recorder.state);
+    console.log('recorder started');
+    stopButton.disabled = false;
+    startButton.disabled = true;
+  });
+
+  // push audio to the empty array
+  recorder.addEventListener('dataavailable', e => {
+    audioChunks.push(e.data);
+  });
+
+  // once "stop" is clicked, send the recording to the player object
+  stopButton.addEventListener('click', () => {
+    // send a signal for recorder to stop
+    recorder.stop();
+    console.log(recorder.state);
+    console.log('recorder stopped');
+
+    // once recorder is stopped, get data from array into a blob, and set it to the player for playback
+    recorder.addEventListener('stop', () => {
+      // create new blob from the recording
+      const audioBlob = new Blob(audioChunks);
+      // clear previous recording
+      audioChunks = [];
+      var audioUrl = URL.createObjectURL(audioBlob);
+      // put recording to the player
+      player.src = audioUrl;
+      console.log('playback ready')
+    })
+
+    stopButton.disabled = true;
+    startButton.disabled = false;
+  });
 };
 
-
-const
+window.onload = mic();
